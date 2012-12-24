609a39bfcf50a635a974b38c108630fadc19a482
Francesco Marangi <f.marangi@mzentrale.de>
2012-12-24 11:38:42 +0100

    Add helper methods to simplify lookup operations

--- src/app/code/community/Mzentrale/GeoIP/Helper/Data.php
+++ src/app/code/community/Mzentrale/GeoIP/Helper/Data.php
@@ -33,5 +33,40 @@
  */
 class Mzentrale_GeoIP_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    
+
+    /**
+     * Get country by IP
+     * 
+     * Returns the ISO code of the country, or false if the IP address
+     * is not valid or cannot be found.
+     * 
+     * @param string $ipAddress
+     * @return string
+     */
+    public function getCountry($ipAddress = null)
+    {
+        if (!$ipAddress) {
+            $ipAddress = Mage::helper('core/http')->getRemoteAddr();
+        }
+        return Mage::getModel('mzgeoip/location')->getCountryByIP($ipAddress);
+    }
+
+    /**
+     * Get country name by IP
+     * 
+     * @param string $ipAddress
+     * @param string $locale
+     * @return string
+     */
+    public function getCountryName($ipAddress = null, $locale = null)
+    {
+        $country = $this->getCountry($ipAddress);
+        if ($country) {
+            if (!$locale) {
+                $locale = Mage::app()->getLocale()->getLocale();
+            }
+            return Mage::app()->getLocale()->getLocale()->getTranslation($country, 'country', $locale);
+        }
+        return false;
+    }
 }
